15b76eb0e6b5699cac085bd112893ecd53368a0e
chantellalvarenga <chantell.alvarenga@gmail.com>
2021-02-28 00:22:32 -0600

    Implementacion de split para obtener el conteo de votos, faltan validaciones y mostrar porcentajes

--- app/src/main/java/sv/edu/udb/menudeejercicios/votos.java
+++ app/src/main/java/sv/edu/udb/menudeejercicios/votos.java
@@ -46,5 +46,47 @@ public class votos extends AppCompatActivity {
     public void ContarVotos (View v) {
 
         //Inicio
+        try { //Siempre usar un try catch para validar campos vacíos desde la obtención de datos
+
+            String VotosText = StringVotos.getText().toString(); //Obtenemos la cadena de votos
+
+            if( VotosText.isEmpty() ) {
+                Toast notificacion = Toast.makeText(this,"¡Datos vacíos! Debe ingresar una serie de votos de la forma: 1,2,1,4,4,3",Toast.LENGTH_LONG);
+                notificacion.show();
+            } else {
+
+                votosArray = VotosText.split(",");   //Separamos cada valor del voto de la cadena en una posicion diferente del array por cada ,
+                int n = votosArray.length; //cantidad de votos
+
+                for(int i=0; i<n; i++){
+                    if( votosArray[i].contains("1") ) {
+                        VotosCandidato1++;
+                    } else if( votosArray[i].contains("2") ) {
+                        VotosCandidato2++;
+                    } else if( votosArray[i].contains("3") ) {
+                        VotosCandidato3++;
+                    } else if( votosArray[i].contains("4") ) {
+                        VotosCandidato4++;
+                    } else {
+                        VotosNulos++;  //Si ingresa un numero que no sea del 1-4
+                    }
+                }
+
+                if( VotosNulos > 0 ){
+                    Toast notificacion = Toast.makeText(this,"Se anularon " + VotosNulos + " votos",Toast.LENGTH_LONG);
+                    notificacion.show();
+                }
+
+                tvVotosCandidato1.setText(VotosCandidato1);
+                tvVotosCandidato2.setText(VotosCandidato2);
+                tvVotosCandidato3.setText(VotosCandidato3);
+                tvVotosCandidato4.setText(VotosCandidato4);
+                tvVotosNulos.setText(VotosNulos);
+            }
+
+        } catch (Exception e ){
+            Toast notificacion2 = Toast.makeText(this, "Ocurrió un error, vuelva a intentarlo", Toast.LENGTH_SHORT);
+            notificacion2.show();
+        }
     }
 }
