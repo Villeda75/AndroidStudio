36cf2208c9cf759563ac72c64b35ef3f6dfa9aa2
chantellalvarenga <chantell.alvarenga@gmail.com>
2021-02-27 20:36:53 -0600

    Codificacion ejercicio 1

--- app/src/main/java/sv/edu/udb/menudeejercicios/ecuacionCuadratica.java
+++ app/src/main/java/sv/edu/udb/menudeejercicios/ecuacionCuadratica.java
@@ -41,6 +41,50 @@ public class ecuacionCuadratica extends AppCompatActivity {
     public void ResultadosActividad (View v) {
 
         //Comenzamos con las validaciones
+        try {
+            //Siempre usar un try catch para validar campos vacíos desde la obtención de datos
 
+            Double A = Double.valueOf(a.getText().toString());
+            Double B = Double.valueOf(b.getText().toString());
+            Double C = Double.valueOf(c.getText().toString());
+
+            if( A == 0 ){ //No se puede dividir entre cero
+
+                Toast notificacion = Toast.makeText(this,"No se puede dividir entre 0. ( a > 0 )",Toast.LENGTH_LONG);
+                notificacion.show();
+
+                //Por si no ve el Toast
+                tvX1.setText("No se puede dividir entre 0. ( A > 0 )");
+
+            } else {
+
+                if ( B == 0 && C == 0 ) {  // 0 dividido entre cualquier valor de A será igual a cero (0/1)=0
+                    X1 = 0.0;
+                    X2 = 0.0;
+
+                } else {  //Se calculan las raices
+                    Double raiz1 = Math.sqrt( ( B*B - 4*A*C ) );
+                    X1 = (-(B)+raiz1)/(2*A);
+                    X2 = (-(B)-raiz1)/(2*A);
+                }
+
+                if( X1.toString() == "NaN" || X2.toString() == "NaN") {   //Si las raices son imaginarias mostramos mensaje
+                    tvX2.setVisibility(View.INVISIBLE);
+                    tvX1.setText("Las soluciones X1 y X2 son imaginarias");
+
+                } else {
+                    tvX2.setVisibility(View.VISIBLE);
+                    tvX1.setText("X1 = " +X1);
+                    tvX2.setText("X2 = " +X2);
+                }
+
+                frameResults.setVisibility(View.VISIBLE);
+            }
+
+        } catch (Exception e ){
+            Toast.makeText(getApplicationContext(), "No se permiten campos vacíos", Toast.LENGTH_SHORT).show();
+            tvX2.setVisibility(View.INVISIBLE); //Se muestran solamente cuando el frame ya está visible.
+            tvX1.setText("No se permiten campos vacíos");
+        }
     }
 }
